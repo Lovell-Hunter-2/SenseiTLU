357f6c60e68ae194c8180dd1af0cb2decc3b0560
Lovell-Hunter-2 <ngothuanbg1612007@gmail.com>
2026-05-28 03:04:37 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'katex/dist/katex.min.css';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
 import Layout from './components/Layout';
