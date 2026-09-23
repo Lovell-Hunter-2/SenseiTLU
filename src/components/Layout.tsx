13451b2ab340ad6069aa52fc35c93c5d231c9ae6
Lovell-Hunter-2 <ngothuanbg1612007@gmail.com>
2026-09-23 11:49:07 +0700

    Adjust padding in main content area

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -442,7 +442,7 @@ export default function Layout() {
       </header>
 
       {/* Main Content */}
-      <main className="flex-1 container mx-auto px-4 py-8">
+      <main className="flex-1 container mx-auto px-2 sm:px-4 py-4 sm:py-8">
         <Outlet />
       </main>
 
